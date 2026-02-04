c8904ced3467b0d417faf6e40138f1066b43f075
Andy <68442103+drixev@users.noreply.github.com>
2026-02-04 10:07:04 -0500

    add allowing host as env variable (#4)

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,29 @@
-import { defineConfig, type PluginOption } from "vite";
+import { defineConfig, loadEnv, type PluginOption } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8081,
-    hmr: {
-      overlay: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const allowedHosts = env.VITE_ALLOWEDHOSTS
+    ? env.VITE_ALLOWEDHOSTS.split(",")
+    : [];
+
+  return {
+    server: {
+      host: "::",
+      port: 8081,
+      hmr: {
+        overlay: false,
+      },
+      allowedHosts,
     },
-    allowedHosts:[
-      "trigonous-tifany-unelaborately.ngrok-free.dev", // ngrok
-      "remedies-echo-representing-tile.trycloudflare.com" // cloudflared
-    ]
-  },
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-}));
+  };
+});
